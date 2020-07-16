e2ac534bb0c8236917b7f2a6e47e890fcefad305
subeen1007 <cc27c@naver.com>
2020-07-16 11:01:12 +0900

    선생님 강좌개설하기 버튼 구현
    선생님이 강좌개설하기 버튼을 누르고 강좌명, 학교, 학년, 반, 선생님이름, 최대수강인원을 입력하면 디비에 저장됨

--- vue-dictation/src/components/StudyWrite.vue
+++ vue-dictation/src/components/StudyWrite.vue
@@ -27,28 +27,33 @@
         <v-card>
           <v-card-title>
           </v-card-title>
+
+        
           <div>
-          <input v-model="message" id="LECTURE_NM" placeholder="강좌이름">
+          <input v-model="lecture.lecture_nm" id="LECTURE_NM" placeholder="강좌이름">
           </div>
           <div>
-          <input v-model="message" id="SCHOOL_CD" placeholder="학교이름">
+          <input v-model="lecture.school_cd" id="SCHOOL_CD" placeholder="학교이름">
           </div>
           <div>
-          <input v-model="message" id="GRADE" placeholder="학년">
+          <input v-model="lecture.grade" id="GRADE" placeholder="학년">
           </div>
           <div>
-          <input v-model="message" id="BAN" placeholder="반">
+          <input v-model="lecture.ban" id="BAN" placeholder="반">
           </div>
           <div>
-          <input v-model="message" id="TEACHER_ID" placeholder="강좌등록 선생님 이름">
+          <input v-model="lecture.teacher_id" id="TEACHER_ID" placeholder="강좌등록 선생님 이름">
           </div>
           <div>
-          <input v-model="message" id="MAX_CNT" placeholder="수강 최대 인원">
+          <input v-model="lecture.max_cnt" id="MAX_CNT" placeholder="수강 최대 인원">
           </div>
           <!--수강신청 및 받아쓰기 날짜 설정 필요 -->
           <v-flex class="text-center">
-        <v-btn width="270" color="primary" x-large @click="sumbit()">등록</v-btn>
-    </v-flex>
+
+          <v-btn width="270" color="primary" x-large @click="sumbit()">등록</v-btn>
+    
+          </v-flex>
+
         <template v-slot:item.actions="{ item }">
           <v-btn class="mr-5" small color="primary" @click="{item}" >학습자료보기</v-btn>
           <v-btn small color="primary" @click="goCource()">시작하기</v-btn>
@@ -57,6 +62,7 @@
       </v-tab-item>
       <v-flex class="text-center">
     </v-flex>
+      
      <v-tab-item>
           <v-card class="mx-auto">
     <v-card-title>
@@ -72,30 +78,47 @@
 
 <script>
 import router from '../router'
+
   export default {
-    data () {
-      return {
-        courseTabs: ["강좌등록", "받아쓰기 등록"],
-        tabs: null,
-        text:'',
-        LECTURE_NM:'',
-        SCHOOL_CD:'',
-        GRADE:'',
-        BAN:'',
-        TEACHER_ID:'',
-        MAX_CNT:'',// 입력자 아이디, 입력일시, 수정아이디, 수정일시 넣기 필요
+    data: () =>({
+      
+      courseTabs: ["강좌등록", "받아쓰기 등록"],
+      tabs: null,
+      text:"",
+      //데이터를 넣기위해선 함수형태여야함(data:()=>({})형태)
+      //값 받아올때 lecture.lecture_nm처럼 .연산자로 값 받아와야함
+      lecture:
+      {
+        year:"",
+        term:"",
+        lecture_no:"",
+        lecture_nm:"",
+        school_cd:"",
+        grade:"",
+        ban:"",
+        enroll_st_ct:"",
+        enroll_ed_ct:"",
+        teacher_id:"",
+        teacher_nm:"",
+        max_cnt:"",// 입력자 아이디, 입력일시, 수정아이디, 수정일시 넣기 필요
+        input_id:"",
+        input_date:"",
+        update_id:"",
+        update_date:""
       }
-    },
+      
+    }),
+    
     methods: {
       sumbit(){
-          for(let item of this.LECTURE_NM + this.SCHOOL_CD + this.GRADE + this.BAN + this.TEACHER_ID + this.MAX_CNT){
-              this.$http.post('/api/lecture', item).then(res =>{
-                  console.log(res);
-                  router.push({name: 'stwr'});
-              }).catch(err=>{
-                  console.error(err);
-              })
-          }
+      
+          this.$http.post('/api/lecture', this.lecture).then(res =>{
+              console.log(res);
+              router.push({name: 'stwr'});
+          }).catch(err=>{
+              console.error(err);
+          })
+          
       },
       goCource() {
         router.push({name: 'stwr'});
